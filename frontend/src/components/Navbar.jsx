e5daa7f377b84d6a288b25922702284f616437e8
Aftab Alam <aaftabalam18191@gmail.com>
2025-06-29 12:50:36 +0530

    add some css properties in the Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,29 +7,32 @@ import {assets} from '../assets/assets';
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400 '>
-        <img src={assets.logo} alt="" />
+        <img  className ='w-44 cursor-pointer' src={assets.logo} alt="" />
 
 
         <ul className='hidden md:flex items-start gap-5 font-medium'>
-            <NavLink> 
-                <li>HOME</li>
-                <hr />
+            <NavLink to='/' > 
+                <li className='py-1'>HOME</li>
+                <hr className='border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink> 
-                <li>ALL DOCTORS</li>
-                <hr />
+
+            <NavLink to='/doctors'> 
+                <li className='py-1'>ALL DOCTORS</li>
+                <hr className='border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink> 
-                <li>ABOUT</li>
-                <hr />
+
+            <NavLink to='/about'> 
+                <li className='py-1'>ABOUT</li>
+                <hr className='border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink> 
-                <li> CONTACT</li>
-                <hr />
+
+            <NavLink to='/contact'> 
+                <li className='py-1'> CONTACT</li>
+                <hr className='border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden' />
             </NavLink>
         </ul>
         <div>
-            <button>Create account</button>
+            <button className='bg-[#5F6FFF] text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
         </div>
     </div>
   )
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss({
+      theme: {
+        extend: {
+          colors: {
+            primary: '#5F6FFF',
+          },
+        },
+      },
+    }),
+  ],
+});
+
+
 
 
 
