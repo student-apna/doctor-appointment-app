a50d08122e3e2b5f1f4f7d71f27f200c20640d8c
Aftab Alam <aaftabalam18191@gmail.com>
2025-06-29 17:08:45 +0530

    creating the header Component and add some css properties

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,6 +2,10 @@
 import logo from './logo.svg'
 import profile_pic from './profile_pic.jpg'
 import dropdown_icon from './dropdown_icon.svg'
+import group_profile from './group_profiles.png'
+import header_img from './header_img.png';
+import arrow_icon from './arrow_icon.svg';
+import doctors_img from './threedoctors_img.png'
 
 
 
@@ -10,6 +14,10 @@ export const assets = {
   
     logo,
     profile_pic,
-    dropdown_icon
+    dropdown_icon,
+    group_profile,
+    header_img,
+    arrow_icon,
+    doctors_img,
     
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,22 +17,22 @@ const Navbar = () => {
         <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink to='/' > 
                 <li className='py-1'>HOME</li>
-                <hr className='border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden' />
+                <hr className='border-none outline-none h-0.5 bg-primary  w-3/5 m-auto hidden' />
             </NavLink>
 
             <NavLink to='/doctors'> 
                 <li className='py-1'>ALL DOCTORS</li>
-                <hr className='border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden' />
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
 
             <NavLink to='/about'> 
                 <li className='py-1'>ABOUT</li>
-                <hr className='border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden' />
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
 
             <NavLink to='/contact'> 
                 <li className='py-1'> CONTACT</li>
-                <hr className='border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden' />
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
         </ul>
         <div className='flex items-center gap-5'>
@@ -50,7 +50,7 @@ const Navbar = () => {
                     </div>
                   </div>
                </div>
-               :  <button onClick={()=>navigate('/login')}  className='bg-[#5F6FFF] text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
+               :  <button onClick={()=>navigate('/login')}  className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
             }
 
             
