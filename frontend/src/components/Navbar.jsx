81fffaa9842acfcc693acf00bf0a66559beff1be
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-05 00:18:25 +0530

    Creating a Appointment page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
                   <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                     <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                         <p onClick={()=>navigate('/my-profile')}  className='hover:text-black cursor-pointer'>My Profile</p>
-                        <p onClick={()=>navigate('/my-appointrments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                        <p onClick={()=>navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
                         <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
                     </div>
                   </div>
