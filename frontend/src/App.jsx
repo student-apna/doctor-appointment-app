37f34f701ab68ace50f5a4f9cf7166533fd1972d
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-05 16:15:30 +0530

    Creating a Email Verify Page and add css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import MyAppointments from './pages/MyAppointments';
 import Appointment from './pages/Appointment';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import EmailVerify from './pages/EmailVerify';
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]' >
@@ -25,6 +26,7 @@ const App = () => {
         <Route path='/my-profile' element={<MyProfile/>} />
         <Route path='/my-appointments' element={<MyAppointments/>} />
         <Route path='/appointment/:docId' element={<Appointment/>} />
+        <Route path='/email-verify' element={<EmailVerify/>} />
 
 
 
---
+++ frontend/src/pages/EmailVerify.jsx
@@ -0,0 +1,62 @@
+import { useRef } from "react";
+
+const EmailVerify = () => {
+  const inputRefs = useRef([]);
+
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData("text");
+    const pasteArray = paste.split("");
+    pasteArray.forEach((char, index) => {
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
+      }
+    });
+  };
+
+  return (
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 rounded-xl text-zinc-600 text-sm shadow-lg">
+        <p className="text-2xl font-semibold">Email Verify OTP</p>
+        <p>Enter the 6-digit code sent to your email id.</p>
+
+        <div className="flex justify-between w-full mb-4" onPaste={handlePaste}>
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <input
+                type="text"
+                maxLength="1"
+                key={index}
+                required
+                className="w-12 h-12 border border-zinc-300 rounded text-center text-lg"
+                ref={(e) => (inputRefs.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+              />
+            ))}
+        </div>
+
+        <button
+          type="submit"
+          className="bg-primary text-white w-full py-2 rounded-md text-base"
+        >
+          Verify Email
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default EmailVerify;
