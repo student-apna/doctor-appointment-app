8d85ad48d6afc51ad0a120898087532485888e3f
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-07 23:18:42 +0530

    setup the multer file for image upload and create endpoints

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
 
 // app config
@@ -21,6 +22,9 @@ app.use(cors());
 app.get('/',(req,res)=>{
     res.send('API is Working Fine aftab');
 })
+
+app.use('/api/admin',adminRouter)
+//localhost:4000/api/admin/add-doctor
 
 app.listen(PORT,()=>{
     console.log(`Server is Running at port ${PORT}`);
