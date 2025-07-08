f6109e9eaf330173a8b1c681134df676c9abef41
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-08 12:56:08 +0530

    Creating a admin login controller function

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -62,9 +62,30 @@ const addDoctor  = async (req,res)=>{
 
   // admin login
 
-  
+  const adminLogin = async (req,res)=>{
+
+     try {
+        const {email,password} = req.body;
+        if(email===process.env.ADMIN_EMAIL && password ===process.env.ADMIN_PASSWORD){
+            // create token
+            const token = jwt.sign(email+password,process.env.JWT_SECRET);
+            res.json({success:true,token})
+
+        }
+        else{
+            return res.json({success:false,message:"Invalid credentails"})
+        }
+        
+        
+     } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+        
+     }
+
+  }
 
 
 
 
-export {addDoctor}
+export {addDoctor,adminLogin}
