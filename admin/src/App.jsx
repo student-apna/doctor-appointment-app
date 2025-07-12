47fe1e70e77ea68d44d5bfb2238a6e8e6c797884
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-12 16:56:38 +0530

    Creating a Navbar for admin pannel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,23 @@
+import { useContext } from "react";
 import Login from "./pages/Login"
 import { ToastContainer, toast } from 'react-toastify';
+import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return (
-    <div>
-      <Login/>
+  const {aToken} = useContext(AdminContext);
+
+
+  return aToken ? (
+    <div className="bg-[#F8F9FD]">
         <ToastContainer />
+        <Navbar/>
     </div>
+  ) :(
+     <>
+       <Login/>
+        <ToastContainer />
+     </>
   )
 }
 
--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -4,7 +4,8 @@ export const AdminContext = createContext();
 
 const AdminContextProvider = (props)=>{
 
-    const[aToken ,setAToken] = useState('');
+    const[aToken ,setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'');
+    // jab localStorage mein token hoga tab login page hidden ho jayega, otherwise login page open hoga
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
     const value = {
