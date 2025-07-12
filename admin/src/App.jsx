9a837f2968f1d5e09eb2c1a34be0555743c37244
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-12 17:30:20 +0530

    Creating the Sidebar component for Admin dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
              <Route path="/admin-dashboard" element={<Dashboard/>} />
              <Route path="/all-appointments" element={<AllAppointments/>} />
              <Route path="/add-doctor" element={<AddDoctor/>} />
-             <Route path="/doctor-list" element={<DoctorsList/>} />
+             <Route path="/doctors-list" element={<DoctorsList/>} />
              
           </Routes>
         </div>
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import { assets } from '../assets/assets';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
+    const {aToken} =  useContext(AdminContext);
   return (
-    <div>Sidebar</div>
+    <div className='min-h-screen bg-white border-r'>
+        {
+            aToken && <ul className='text-[#515151] mt-5'>
+                 <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4  border-[#5F6FFF]':''}`} to={'/admin-dashboard'}>
+                    <img src={assets.home_icon} alt="" /> 
+                    <p>Dashboard</p>
+                 </NavLink>
+
+                  <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4  border-[#5F6FFF]':''}`} to={'/all-appointments'}>
+                    <img src={assets.appointment_icon} alt="" /> 
+                    <p>Appointments</p>
+                 </NavLink>
+
+                  <NavLink  className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4  border-[#5F6FFF]':''}`} to={'/add-doctor'}>
+                    <img src={assets.add_icon} alt="" /> 
+                    <p>Add Doctor</p>
+                 </NavLink>
+
+                  <NavLink  className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4  border-[#5F6FFF]':''}`} to={'/doctors-list'}>
+                    <img src={assets.people_icon} alt="" /> 
+                    <p>Doctors List</p>
+                 </NavLink>
+                 
+            </ul>
+        }
+
+
+    </div>
   )
 }
 
