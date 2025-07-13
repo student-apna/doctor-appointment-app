0275cdb45857c1e9724834e1d063cd14bdcc3ca9
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-13 19:25:05 +0530

    Creating a loginUser controller and endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,4 +75,45 @@ import validator from 'validator';
     }
 }
 
-export {registerUser};
+const loginUser = async (req,res)=>{
+    const {email,password} = req.body;
+
+    if(!email || !password){
+        return res.json({success:false,message:"Email and Password are required"});
+    }
+    try {
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.json({success:false ,message:"User not found"});
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.json({success:false,message:'Invalid Password'});
+        }
+
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'});
+
+        res.cookie('token',token,{
+            httpOnly:true,
+             // secure :process.env.NODE_ENV==='production' ,
+            // sameSite:process.env.NODE_ENV==='production'?
+            // 'none':'strict',
+             secure :true,
+             sameSite :'none',
+            
+            maxAge:7 * 24 * 60 * 60 * 1000
+        })
+
+        return res.json({
+            success:true
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+        
+    }
+}
+
+export {registerUser,loginUser};
