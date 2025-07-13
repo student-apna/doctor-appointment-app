81310f13d7757b2a48c3ac690d48e0994b0d7a24
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-13 12:16:58 +0530

    Creating the Doctors page for the Admin Pannel

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { AdminContext } from '../../context/AdminContext'
 
 const DoctorsList = () => {
+
+  const {doctors,aToken,getAllDoctors} = useContext(AdminContext);
+
+   useEffect(()=>{
+    if(aToken){
+      getAllDoctors();
+    }
+
+   },[aToken])
+
   return (
-    <div>DoctorsList</div>
+    <div>
+       <h1>All Doctors</h1>
+       <div>
+        {
+          doctors.map((item,index)=>(
+            <div key={index}>
+              <img src={item.image} alt="" />
+              <div>
+                <p>{item.name}</p>
+                <p>{item.speciality}</p>
+                <div>
+                  <input type="checkbox" checked={item.available} />
+                  <p>Available</p>
+                </div>
+              </div>
+
+            </div>
+          ))
+        }
+
+       </div>
+    </div>
   )
 }
 
