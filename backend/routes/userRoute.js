5c173ee4151d3507726be2e92d83ac1f57a19c4b
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-13 20:49:24 +0530

    creating the userAuth middleware for extarcting the id from cookies

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/userController.js';
+import { loginUser, logoutUser, registerUser } from '../controllers/userController.js';
 
 
 const userRouter = express.Router();
 
 userRouter.post('/register',registerUser);
-userRouter.post('/login',loginUser)
+userRouter.post('/login',loginUser);
+userRouter.post('/logout',logoutUser)
 
 
 export default userRouter;
