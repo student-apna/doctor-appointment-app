7cf21a8304bd19f341043b53ffff17c6ecd782db
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-13 20:42:40 +0530

    Creating a logoutUser controller function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -116,4 +116,20 @@ const loginUser = async (req,res)=>{
     }
 }
 
-export {registerUser,loginUser};
+const logoutUser = async (req,res) =>{
+    try {
+        res.clearCookie('token',{
+            httpOnly:true,
+            secure :process.env.NODE_ENV==='production' ,
+            sameSite:process.env.NODE_ENV==='production'?
+            'none':'strict',
+        })
+
+        return res.json({success:true,message:"Logged Out"});
+        
+    } catch (error) {
+        return res.json({success:false,message:error.message})
+    }
+}
+
+export {registerUser,loginUser,logoutUser};
