2a01c8e7c8d242e67f2ebad0ec8c3235290fc95e
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-16 13:06:19 +0530

    write functions for verify email

--- frontend/src/pages/EmailVerify.jsx
+++ frontend/src/pages/EmailVerify.jsx
@@ -1,7 +1,16 @@
 import { useRef } from "react";
+import axios from "axios";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const EmailVerify = () => {
+  axios.defaults.withCredentials = true;
   const inputRefs = useRef([]);
+  const {backendUrl,token,userData,loadUserProfileData} = useContext(AppContext);
+  const navigate = useNavigate();
 
   
   const handleInput = (e, index) => {
@@ -26,9 +35,37 @@ const EmailVerify = () => {
     });
   };
 
+
+  const onSubmitHandler = async (e)=>{
+     try {
+      e.preventDefault();
+      const otpArray = inputRefs.current.map(e=>e.value);
+      const otp = otpArray.join('');
+      const {data} = await axios.post(backendUrl+'/api/user/verify-account',{otp},{headers:{token}});
+      if(data.success){
+         toast.success(data.message);
+         await loadUserProfileData();
+         navigate('/')
+      }
+      else{
+        toast.error(data.message);
+      }
+      
+     } catch (error) {
+       toast.error(error.message)
+      
+     }
+  }
+
+ useEffect(() => {
+  if (token && userData?.isAccountVerified) {
+    navigate('/');
+  }
+}, [token, userData?.isAccountVerified]);
+
   
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 rounded-xl text-zinc-600 text-sm shadow-lg">
         <p className="text-2xl font-semibold">Email Verify OTP</p>
         <p>Enter the 6-digit code sent to your email id.</p>
