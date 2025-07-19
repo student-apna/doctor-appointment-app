905b00896ed817714ea857148e935b00ec56133a
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-19 11:09:38 +0530

    creating the Table for display the Appointment data

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -1,8 +1,38 @@
+import { useContext, useEffect } from "react"
+import { AdminContext } from "../../context/AdminContext"
 
 const AllAppointments = () => {
+
+  const {aToken,appointments,getAllAppointments} = useContext(AdminContext);
+  
+  useEffect(()=>{
+    if(aToken){
+      getAllAppointments();
+    }
+
+  },[aToken])
+
+
+
+
   return (
-    <div>AllAppointments</div>
+    <div className="w-full max-w-6xl m-5">
+      <p className="mb-3 text-lg font-medium">All Appointments</p>
+      <div className="bg-white  rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll">
+        <div className="hidden sm:grid grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b">
+          <p>#</p>
+          <p>Patient</p>
+          <p>Age</p>
+          <p>Date & Time</p>
+          <p>Doctor</p>
+          <p>Fees</p>
+          <p>Actions</p> 
+        </div>
+      </div>
+
+    </div>
   )
 }
 
 export default AllAppointments
+
