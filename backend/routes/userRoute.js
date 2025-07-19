3bb89dbcd8e90534250f4fd373c24ae2ca0d9146
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-19 08:58:13 +0530

    Creating a route for resetPassword

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getProfile, loginUser,  registerUser,sendVerifyOtp,updateProfile, verifyEmail ,bookAppointment,appointmentList,cancelAppointment,confirmPayment,sendResetOtp} from '../controllers/userController.js';
+import { getProfile, loginUser,  registerUser,sendVerifyOtp,updateProfile, verifyEmail ,bookAppointment,appointmentList,cancelAppointment,confirmPayment,sendResetOtp,resetPassword} from '../controllers/userController.js';
 import userAuth from '../middlewares/authUser.js';
 import upload from '../middlewares/multer.js';
 
@@ -17,6 +17,7 @@ userRouter.get('/appointments',userAuth,appointmentList);
 userRouter.post('/cancle-appointment',userAuth,cancelAppointment);
 userRouter.post('/confirm-payment',userAuth,confirmPayment);
 userRouter.post('/send-reset-otp',sendResetOtp);
+userRouter.post('/reset-password',resetPassword);
 
 
 export default userRouter;
