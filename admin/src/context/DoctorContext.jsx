54499925633594be77221bf39330ea857ccace19
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-20 18:24:15 +0530

    Ceating the getDashdata function to get the dashData in the doctorContext file

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -12,14 +12,16 @@ const DoctorContextProvider = (props)=>{
 
      const [appointments,setAppointments] = useState([]);
 
+     const [dashData,setDashData] = useState(false);
+
      const getAppointments = async()=>{
         try {
 
             const {data} = await axios.get(backendUrl+'/api/doctor/appointments',{headers:{dToken}});
 
             if(data.success){
-                setAppointments(data.appointments.reverse());
-                console.log(data.appointments.reverse())
+                setAppointments(data.appointments);
+                console.log(data.appointments)
             }
             else{
                 toast.error(data.message);
@@ -77,13 +79,33 @@ const DoctorContextProvider = (props)=>{
  }
 
 
+  const getDashData = async()=>{
+     try {
+
+        const {data} = await axios.get(backendUrl+'/api/docto/dashboard',{headers:{dToken}});
+        if(data.success){
+          setDashData(data.dashData);  
+        }
+        else{
+            toast.error(data.message);
+        }
+        
+     } catch (error) {
+        console.log(error);
+       toast.error(error.message);
+        
+     }
+  }
+
+
     const value = {
         dToken,setDToken,
         backendUrl,
         appointments,setAppointments,
         getAppointments,
         completeAppointment,
-        cancelAppointment
+        cancelAppointment,
+        dashData,setDashData,getDashData
 
         
     }
