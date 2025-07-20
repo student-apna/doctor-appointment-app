cb73b798291e30b59376cac19795cada2ee53557
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-20 12:02:17 +0530

    call the backend api for the doctor login in the Login page

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -3,12 +3,15 @@ import { assets } from '../assets/assets'
 import { AdminContext } from '../context/AdminContext';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { DoctorContext } from '../context/DoctorContext';
 
 const Login = () => {
     const [state, setState] = useState('Admin');
     const { setAToken, backendUrl } = useContext(AdminContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    
+    const {setDToken} = useContext(DoctorContext);
 
     const onSubmitHandler = async (event) => {
           event.preventDefault();
@@ -27,7 +30,18 @@ const Login = () => {
 
 
             } else{
-                // Doctor state
+                // Doctor state // login
+                const {data} = await axios.post(backendUrl+'/api/doctor/login',{email,password});
+
+                  if(data.success){
+                    localStorage.setItem('dToken',data.token);
+                    setDToken(data.token);
+                    console.log(data.token);
+                    
+                }
+                else{
+                    toast.error(data.message);
+                }
             }
             
           } catch (error) {
