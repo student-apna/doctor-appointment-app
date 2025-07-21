00e84c347c22e7120d807f351fa375a40ddbb285
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-21 13:05:02 +0530

    Add the functionality when doctor complete the Appointment then on the user Appointments it showa completed

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -1,13 +1,44 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { DoctorContext } from "../../context/DoctorContext"
 import { AppContext } from "../../context/AppContext";
 import { useEffect } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 
 const DoctorProfile = () => {
   
-  const {dToken,  profileData,setProfileData,getProfileData} = useContext(DoctorContext);
-  const {currency,backendUrl} = useContext(AppContext);
+  const {dToken,  profileData,setProfileData,getProfileData,backendUrl} = useContext(DoctorContext);
+  const {currency} = useContext(AppContext);
+
+  const [isEdit,setIsEdit] = useState(false);
+
+  const updateProfile = async ()=>{
+
+     try {
+
+      const updateData = {
+        address:profileData.address,
+        fees :profileData.fees,
+        available :profileData.available
+      }
+
+      const {data} = await axios.post(backendUrl+'/api/doctor/update-profile',updateData,{headers:{dToken}});
+
+      if(data.success){
+        toast.success(data.message);
+        setIsEdit(false)
+        getProfileData()
+      }else{
+        toast.error(data.message);
+      }
+      
+     } catch (error) {
+        toast.error(error.message);
+        console.log(error);
+      
+     }
+  }
 
   useEffect(()=>{
     if(dToken){
@@ -48,25 +79,28 @@ const DoctorProfile = () => {
                </div>
                
                 <p className="text-gary-600 font-medium mt-4">
-                  Appointment fee : <span className="text-gray-800">{currency} {profileData.fees}</span>
+                  Appointment fee : <span className="text-gray-800">{currency} {isEdit? <input type="number" value={profileData.fees} onChange={(e)=>setProfileData(prev=>({...prev,fees:e.target.value}))} /> : profileData.fees}</span>
                 </p>
 
                 <div className="flex-gap-2 py-2">
                   <p>Address:</p>
                   <p className="text-sm">
-                    {profileData.address.line1} <br />
-                    {profileData.address.line2}
+                      { isEdit ? <input type="text" value={profileData.address.line1}  onChange={(e)=>setProfileData(prev=>({...prev,address:{...prev.address,line1:e.target.value}}))} />  :profileData.address.line1} <br />
+                       { isEdit ? <input type="text" value={profileData.address.line2}  onChange={(e)=>setProfileData(prev=>({...prev,address:{...prev.address,line2:e.target.value}}))} /> :profileData.address.line2}
                   </p>
                 </div>
 
                 <div className="flex gap-1 pt-2">
-                  <input type="checkbox" />
+                  <input onChange={()=> isEdit&& setProfileData(prev => ({...prev,available:!prev.available}))} checked={profileData.available} type="checkbox" />
                   <label htmlFor="">Available</label>
                 </div>
 
-                <button className="px-4 py-1 border border-[#5F6FFF] text-sm rounded-full mt-5 hover:bg-[#5F6FFF] hover:text-white transition-all">Edit</button>
+                { isEdit
 
+                ? <button onClick={updateProfile} className="px-4 py-1 border border-[#5F6FFF] text-sm rounded-full mt-5 hover:bg-[#5F6FFF] hover:text-white transition-all">Save</button>
+                :<button onClick={()=>setIsEdit(true)} className="px-4 py-1 border border-[#5F6FFF] text-sm rounded-full mt-5 hover:bg-[#5F6FFF] hover:text-white transition-all">Edit</button>
 
+                }
   
           </div>
         </div>
